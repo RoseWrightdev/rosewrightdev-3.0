a42e13643a3e8ab63dcb7e95ae38d3192b9b1653
Rose <rosesophiewright@gmail.com>
2022-07-29 12:48:25 -0500

    Layout Folder Added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Layout from '../conponents/layout'
-import Thesis from '../conponents/Thesis/Thesis'
-import Body_1 from '../conponents/Body_1/Body_1'
-import Body_2 from '../conponents/Body_2/Body_2'
-import Body_3 from '../conponents/Body_3/Body_3'
-import Conclusion from '../conponents/Conclusion/Conclusion'
+import Layout from '../layout/layout'
+import Thesis from '../layout/Thesis/Thesis'
+import Body_1 from '../layout/Body_1/Body_1'
+import Body_2 from '../layout/Body_2/Body_2'
+import Body_3 from '../layout/Body_3/Body_3'
+import Conclusion from '../layout/Conclusion/Conclusion'
 
 export default function Home() {
   return (
