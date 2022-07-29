d34aa8f9a0f2524aad236b520a03c5e3bce9855c
rosepetaldrift <rosesophiewright@gmail.com>
2022-07-29 10:12:00 -0500

    Head info changes + layout conponent

--- conponents/layout.jsx
+++ conponents/layout.jsx
@@ -1,6 +1,5 @@
-import Footer from './Footer';
-import Navbar from './navbar';
-
+import Footer from "./Footer/Footer";
+import Navbar from "./Navbar/Navbar";
 const Layout = ({ children }) => {
   return (
     <div>
