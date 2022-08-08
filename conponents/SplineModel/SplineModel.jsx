2a1aa779d4760bcf17f05a1530ce3bdeee7281cc
rosepetaldrift <rosesophiewright@gmail.com>
2022-08-07 22:39:03 -0500

    Tailwind Darkmode

--- conponents/SplineModel/SplineModel.jsx
+++ conponents/SplineModel/SplineModel.jsx
@@ -3,8 +3,9 @@ import Spline from '@splinetool/react-spline';
 
 export default function SplineModel() {
   return (
-    <div className={styles.spline}>
-      <Spline scene="https://draft.spline.design/zGk4SoOKe92prb6O/scene.splinecode" />
+    <div className={styles.spline}>{/*
+      <Spline scene="https://draft.spline.design/zGk4SoOKe92prb6O/scene.splinecode" />*/
+    }
     </div>
   )
 }
--- layout/Navbar/Navbar.jsx
+++ layout/Navbar/Navbar.jsx
@@ -1,14 +1,22 @@
 import styles from "./styles.module.css"
 import { IoMdRose } from "react-icons/io";
 
+const Logo = () => {
+  return (
+    <div className={styles.logo}>
+      <IoMdRose className={styles.rosewrightdevIcon} size={16}/>
+      <h1 className={styles.rosewrightdev}>RoseWright.dev</h1>
+    </div>
+  )
+}
+
 const Navbar = () => {
   return (
     <span>
       <nav className={styles.navbar}>
-        <IoMdRose className={styles.rosewrightdevIcon} size={16}/>
-        <h1 className={styles.rosewrightdev}>RoseWright.dev</h1>
+        <Logo />
       </nav>
-      <hr />
+      <hr className={styles.linebreak} />
     </span>
   )
 }
