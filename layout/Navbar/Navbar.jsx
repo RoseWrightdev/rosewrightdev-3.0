474d5b65515a54d0fd9ab6f7b59595475607b6e2
rosepetaldrift <rosesophiewright@gmail.com>
2022-08-08 19:38:37 -0500

    Working Darkmode hook

--- layout/Navbar/Navbar.jsx
+++ layout/Navbar/Navbar.jsx
@@ -1,15 +1,22 @@
 import styles from "./styles.module.css"
 import { IoMdRose } from "react-icons/io";
+import {
+  FaMoon,
+  FaSun,
+} from 'react-icons/fa';
+import useDarkMode from '../../hooks/useDarkMode';
 
+//RoseWright.dev Logo
 const Logo = () => {
   return (
     <div className={styles.logo}>
       <IoMdRose className={styles.rosewrightdevIcon} size={24}/>
-      <h1 className={styles.rosewrightdev}>RoseWright.dev</h1>
+      <h1 className='font-semibold tracking-tight my-1 text-lg text-black-mid dark:text-red'>RoseWright.dev</h1>
     </div>
   )
 }
 
+//Narbar text Items
 const NarbarItem = ({text = 'TEXT PROP NOT FOUND'}) => {
 
   return (
@@ -19,17 +26,34 @@ const NarbarItem = ({text = 'TEXT PROP NOT FOUND'}) => {
   )
 }
 
+//Left Group
 const GroupOne = ({children}) => {
   return (
   <div className="flex">{children}</div>
   )
 }
 
+//Right Group
 const GroupTwo = ({children}) => {
   return (
   <div className="flex">{children}</div>
   )
 }
+
+//Theme Icon switch
+const ThemeIcon = () => {
+  const [darkTheme, setDarkTheme] = useDarkMode();
+  const handleMode = () => setDarkTheme(!darkTheme);
+  return (
+    <span onClick={handleMode}>
+      {darkTheme ? (
+        <FaSun size='24'  className={styles.rosewrightdevIcon}  />
+      ) : (
+        <FaMoon size='24'  className={styles.rosewrightdevIcon}  />
+      )}
+    </span>
+  );
+};
 
 const Navbar = () => {
   return (
@@ -46,7 +70,7 @@ const Navbar = () => {
             </div>
         </GroupOne>
         <GroupTwo>
-          test
+          <ThemeIcon />
         </GroupTwo>
       </nav>
       <hr className={styles.linebreak} />
