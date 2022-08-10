ecb429ae8ac8cc0a8e871b53c6fac564ca9ca1fd
rosepetaldrift <rosesophiewright@gmail.com>
2022-08-09 19:07:11 -0500

    bg

--- layout/Body_2/Body_2.jsx
+++ layout/Body_2/Body_2.jsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.css"
 
 const Body_2 = () => {
   return (
-    <div className='bg-gray dark:bg-black-off h-[100vh]'></div>
+    <div className='bg-gray dark:bg-black-offlight h-[100vh]'></div>
   )
 }
 
--- layout/Conclusion/Conclusion.jsx
+++ layout/Conclusion/Conclusion.jsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.css"
 
 const Conclusion = () => {
   return (
-    <div className='bg-gray dark:bg-black-off h-[100vh]'></div>
+    <div className='bg-gray dark:bg-black-offlight h-[100vh]'></div>
   )
 }
 
