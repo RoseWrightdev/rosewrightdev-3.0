dced3f123fc9fc99cc4a5666079ae731cbbc63be
Rose <rosesophiewright@gmail.com>
2022-08-20 16:30:05 -0500

    menu State added

--- layout/Navbar/Navbar.jsx
+++ layout/Navbar/Navbar.jsx
@@ -12,11 +12,10 @@ const Logo = () => {
   return (
     <div className={styles.logo}>
       <div className={styles.rosewrightdevIcon}>
-      <svg width="24" height="24" viewBox="0 0 10 10" fill="none">
-<path d="M4.35207 1.58464C4.58926 1.00454 5.41074 1.00454 5.64793 1.58464L6.61785 3.95678C6.64181 4.01538 6.67362 4.07046 6.71239 4.12051L8.28175 6.14656C8.66554 6.64202 8.2548 7.35344 7.63382 7.2688L5.09453 6.92271C5.0318 6.91416 4.9682 6.91416 4.90547 6.92271L2.36618 7.2688C1.7452 7.35344 1.33446 6.64202 1.71825 6.14656L3.28761 4.12051C3.32638 4.07046 3.35819 4.01538 3.38215 3.95678L4.35207 1.58464Z" fill="#FF6884"/>
-<rect x="4" y="4" width="2" height="2" rx="1" fill="white"/>
-</svg>
-
+        <svg width="24" height="24" viewBox="0 0 10 10" fill="none">
+          <path d="M4.35207 1.58464C4.58926 1.00454 5.41074 1.00454 5.64793 1.58464L6.61785 3.95678C6.64181 4.01538 6.67362 4.07046 6.71239 4.12051L8.28175 6.14656C8.66554 6.64202 8.2548 7.35344 7.63382 7.2688L5.09453 6.92271C5.0318 6.91416 4.9682 6.91416 4.90547 6.92271L2.36618 7.2688C1.7452 7.35344 1.33446 6.64202 1.71825 6.14656L3.28761 4.12051C3.32638 4.07046 3.35819 4.01538 3.38215 3.95678L4.35207 1.58464Z" fill="#FF6884"/>
+          <rect x="4" y="4" width="2" height="2" rx="1" fill="white"/>
+        </svg>
       </div>
       <h1 className='font-semibold tracking-tight my-1 text-lg text-black dark:text-white'>RoseWright.dev</h1>
     </div>
@@ -65,10 +64,38 @@ const ThemeIcon = () => {
   );
 };
 
-const Navbar = () => {
+//menu
+const MenuBackground = (props) => {
   return (
-    <span>
-      <div className={styles.desktop}>
+  <div className={styles.mobile}>
+    <div className='h-[100vh] w-[100vw] fixed' onClick={props.onClick}></div>
+  </div>
+  )
+}
+const Menu = (props) => {
+  return (
+  <div className={styles.mobile}>
+    <div className='h-[100vh] w-[100vw] bg-gray-800 fixed' onClick={props.onClick}>Menu test</div>
+  </div>
+  )
+}
+
+const Navbar = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  
+  function menuHandler() {
+    setMenuIsOpen(true);
+  }
+
+  function closeMenuHandler() {
+    setMenuIsOpen(false);
+  }
+
+  return (
+  <span>
+  {/*Desktop*/}
+    <div className={styles.desktop}>
       <nav className='py-2 flex flex-row justify-between bg-white dark:bg-black-900'>
         <div className="flex">
           <Logo />
@@ -87,21 +114,27 @@ const Navbar = () => {
         </div>
       </nav>
       <hr className={styles.linebreak} />
-      </div>
-      <div className={styles.mobile}>
-          <nav className='py-2 flex flex-row justify-between bg-white dark:bg-black-900'>
-            <div className="flex mx-auto">
-              <LogoSmall />
-            </div>
-            <div className="flex">
-              <div className="bg-red-light h-auto w-auto rounded-full hover:rounded-xl p-[7px] mr-8 cursor-pointer transition-all  ease-in-out border-[1px] border-red shadow-lg">
-                <IoMenuOutline size='24' className='text-red' />
-              </div>
-            </div>
-          </nav>
-        <hr className={styles.linebreak} />
-      </div>
-    </span>
+    </div>
+
+  {/*mobile*/}
+    {/*Menu*/}
+    {menuIsOpen && <Menu onClick={closeMenuHandler}/>}
+    {menuIsOpen && <MenuBackground onClick={closeMenuHandler}/>}
+    <div className={styles.mobile}>
+        <nav className='py-2 flex flex-row justify-between bg-white dark:bg-black-900'>
+          <div className="flex mx-auto">
+            <LogoSmall />
+          </div>
+          <div className="flex">
+            <button className="bg-red-light h-auto w-auto rounded-full hover:rounded-xl p-[7px] mr-4 cursor-pointer transition-all  ease-in-out" onClick={menuHandler}>
+              <IoMenuOutline size='24' className='text-black' />
+            </button>
+          </div>
+        </nav>
+      <hr className={styles.linebreak} />
+    </div>
+  
+  </span>
   )
 }
 
