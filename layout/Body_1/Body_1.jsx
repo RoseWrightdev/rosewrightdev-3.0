1c1b46c042afcdadef878e987272737c41b9589b
rosepetaldrift <rosesophiewright@gmail.com>
2022-08-30 01:34:28 -0500

    Images

--- layout/Body_1/Body_1.jsx
+++ layout/Body_1/Body_1.jsx
@@ -1,3 +1,6 @@
+//next
+import Image from 'next/image'
+
 //styles
 import styles from './styles.module.css'
 import global from '../global.module.css'
@@ -5,6 +8,12 @@ import global from '../global.module.css'
 //Svgs
 import NextSvg from './Svgs/NextSvg'
 import TWSvg from './Svgs/TWSvg'
+
+//images
+import next from './images/next.png'
+import tailwindcss from './images/tailwindcss.png'
+import blueback from './images/blueback.png'
+
 const Body_1 = () => {
   return (
     <span>
@@ -12,13 +21,18 @@ const Body_1 = () => {
         <div className={styles.flexbox}>
           <div className={styles.svgcontainer}>
             <br/>
-              <div className='flex align-center justify-center mt-10'>
-                <div>
-                  <NextSvg/>
+              <div className={'flex align-center justify-center mt-10 ' + styles.blueback} >
+                <div className={' ' + styles.techimage}>
+                  <Image src={next} alt='test' layout="responsive"/>
+                </div>
+              </div>
+              <br/>
+              <div className={' ' + styles.tech}>
+                <div className={' ' + styles.techimage}>
+                  <Image src={tailwindcss} alt='test' layout="responsive"/>
                 </div>
               </div>
             <br/>
-            <TWSvg/>
           </div>
         </div>
       </div>
--- layout/Thesis/Thesis.jsx
+++ layout/Thesis/Thesis.jsx
@@ -29,7 +29,7 @@ const Thesis = () => {
         <div className={'p-8 '  + styles.image}>
           <Image src={Rw} alt='A pink blue and white 3d representaion of a online admin control dashboard.' className={' ' + styles.imageSrc}/>
         </div>
-        <div className={'bg-white dark:bg-black-500 ' + styles.textcontainer}>
+        <div className={'bg-white dark:bg-black-800 ' + styles.textcontainer}>
           <h1 className={'dark:text-white ' + styles.text}> <span className={'whitespace-nowrap '}> I help people build </span> <br/> <span className={' ' + styles.pink}>interactive</span> and <span className={styles.blue}>compelling</span> <span className={styles.under}>websites!</span></h1>
           <br/>
           <h2 className={'dark:text-white ' }>I work with a strong focus on simplicity, accessibility <br/> and responsive design.</h2>
