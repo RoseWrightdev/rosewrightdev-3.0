7ced2f8270803ae5456d94698d0dc6dd30eb8610
rosepetaldrift <rosesophiewright@gmail.com>
2022-08-30 10:26:37 -0500

    Added text

--- layout/Body_1/Body_1.jsx
+++ layout/Body_1/Body_1.jsx
@@ -18,8 +18,8 @@ const Body_1 = () => {
   return (
     <span>
       <div className={'bg-white dark:bg-black-500 ' + styles.container}>
+        <h1 className={'pb-5 ' + styles.header}>I work with technologies like </h1>
         <div className={styles.flexbox}>
-          <div className={styles.svgcontainer}>
             <br/>
               <div className={'flex align-center justify-center mt-10 ' + styles.blueback} >
                 <div className={' ' + styles.techimage}>
@@ -33,7 +33,6 @@ const Body_1 = () => {
                 </div>
               </div>
             <br/>
-          </div>
         </div>
       </div>
     </span>
