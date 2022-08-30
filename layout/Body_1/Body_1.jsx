e9f2cab55052761e9769a6c3430a10b0fb37d2eb
Rose <rosesophiewright@gmail.com>
2022-08-29 21:33:12 -0500

    Thesis changes

--- layout/Body_1/Body_1.jsx
+++ layout/Body_1/Body_1.jsx
@@ -3,23 +3,25 @@ import styles from './styles.module.css'
 import global from '../global.module.css'
 
 //Svgs
-import Team from './Svgs/Team'
-
+import NextSvg from './Svgs/NextSvg'
+import TWSvg from './Svgs/TWSvg'
 const Body_1 = () => {
   return (
     <span>
-      
-    <div className={'bg-white dark:bg-black-500 ' + styles.container}>
-      <div className={styles.flexbox}>
-        <div className={styles.header}>
-          <br/>
-          <h1 className={styles.text}></h1>
-          <br/>
-          <br/>
-          <br/>
+      <div className={'bg-white dark:bg-black-500 ' + styles.container}>
+        <div className={styles.flexbox}>
+          <div className={styles.svgcontainer}>
+            <br/>
+              <div className='flex align-center justify-center mt-10'>
+                <div>
+                  <NextSvg/>
+                </div>
+              </div>
+            <br/>
+            <TWSvg/>
+          </div>
         </div>
       </div>
-    </div>
     </span>
   )
 }
--- layout/Thesis/Thesis.jsx
+++ layout/Thesis/Thesis.jsx
@@ -29,13 +29,13 @@ const Thesis = () => {
         <div className={'p-8 '  + styles.image}>
           <Image src={Rw} alt='A pink blue and white 3d representaion of a online admin control dashboard.' className={' ' + styles.imageSrc}/>
         </div>
-        <div className={'' + styles.textcontainer}>
+        <div className={'bg-white dark:bg-black-500 ' + styles.textcontainer}>
           <h1 className={'dark:text-white ' + styles.text}> <span className={'whitespace-nowrap '}> I help people build </span> <br/> <span className={' ' + styles.pink}>interactive</span> and <span className={styles.blue}>compelling</span> <span className={styles.under}>websites!</span></h1>
           <br/>
           <h2 className={'dark:text-white ' }>I work with a strong focus on simplicity, accessibility <br/> and responsive design.</h2>
           <button className={'dark:border-white ' + styles.button}>
             <div className='dark:text-white flex'>
-              learn more <div className='my-auto m-1 mr-0 ml-3'><MdArrowDownward /></div>
+              learn more <div className='my-auto m-1 mr-0 ml-3 font-medium'><MdArrowDownward /></div>
             </div>
           </button>
         </div>
