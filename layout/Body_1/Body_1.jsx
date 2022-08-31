ebd10ab41168ebde80e94dd514b0355e57d17fe2
Rose <rosesophiewright@gmail.com>
2022-08-31 13:22:51 -0500

    Body_1 reset

--- layout/Body_1/Body_1.jsx
+++ layout/Body_1/Body_1.jsx
@@ -14,25 +14,33 @@ import next from './images/next.png'
 import tailwindcss from './images/tailwindcss.png'
 import blueback from './images/blueback.png'
 
+//hooks
+import { useState } from 'react'
+
+const Slide_1 = () => {
+  return (
+    <div className={'flex align-center justify-center mt-10 ' + styles.blueback} >
+      <div className={' ' + styles.techimage}>
+        <Image src={next} alt='test' layout="responsive"/>
+      </div>
+    </div>
+  )
+}
+
+
+
 const Body_1 = () => {
+  const [setSlide, isSetSlide] = useState(1)
+
+  const nextSlide = () => {
+    
+  }
+
   return (
     <span>
-      <div className={'bg-white dark:bg-black-500 ' + styles.container}>
-        <h1 className={'pb-5 ' + styles.header}>I work with technologies like </h1>
+      <div className={'bg-gray-800 dark:bg-black-500 ' + styles.container}>
         <div className={styles.flexbox}>
-            <br/>
-              <div className={'flex align-center justify-center mt-10 ' + styles.blueback} >
-                <div className={' ' + styles.techimage}>
-                  <Image src={next} alt='test' layout="responsive"/>
-                </div>
-              </div>
-              <br/>
-              <div className={' ' + styles.tech}>
-                <div className={' ' + styles.techimage}>
-                  <Image src={tailwindcss} alt='test' layout="responsive"/>
-                </div>
-              </div>
-            <br/>
+     
         </div>
       </div>
     </span>
