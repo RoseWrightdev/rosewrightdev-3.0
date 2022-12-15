71783137996719df92a9dc4c9c5f1bdde87e7bfa
rosepetaldrift <rosesophiewright@gmail.com>
2022-12-14 20:29:25 -0600

    Python

---
+++ Python/DataExample.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+
+# Make data
+X = np.arange(-2.5, 0.75, 0.25)
+Y = np.arange(-1.5, 5, 0.5)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.cos(R)
+
+# Plot the surface
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.plot_surface(X, Y, Z, vmin=Z.min() * 1.5, cmap=cm.Reds)
+
+ax.set(xticklabels=[],
+       yticklabels=[],
+       zticklabels=[])
+
+ax.set_facecolor("#595867")
+
+plt.show()
--- layout/Body_1/Body_1.jsx
+++ layout/Body_1/Body_1.jsx
@@ -9,10 +9,24 @@ import global from '../global.module.css'
 import NextSvg from './Svgs/NextSvg'
 import TWSvg from './Svgs/TWSvg'
 
+//images
+import vis from './images/vis.gif'
+
 const Body_1 = () => {
    return (
     <span>
-     
+        <div className={'bg-white dark:bg-black-500 ' + styles.container}>
+          <div className={styles.flexbox}>
+          <div className={'p-8 '  + styles.image}>
+            <Image src={vis} alt='pink blue and white 3d representaion of a online admin control dashboard.' className={' ' + styles.imageSrc}/>
+          </div>
+            <div className={' ' + styles.textcontainer}>
+            <h1 className={'dark:text-white ' + styles.text}> <span className={'whitespace-nowrap '}> Data Visualization</span> <br/> with <span className={' ' }>Javascript</span> and <span className={' '}>Python</span></h1>
+            <br/>
+            <h2 className={'dark:text-white ' }>Using platforms like Three.js and Matplotlip.</h2>
+          </div>
+      </div>
+        </div>
     </span>
   )
 }
