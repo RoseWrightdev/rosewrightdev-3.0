3e7fabb5187ddb0d7977975804a5a043fbde57ac
rosepetaldrift <rosesophiewright@gmail.com>
2022-12-15 15:44:06 -0600

    Dark Mode Gif

--- layout/Body_1/Body_1.jsx
+++ layout/Body_1/Body_1.jsx
@@ -11,21 +11,25 @@ import TWSvg from './Svgs/TWSvg'
 
 //images
 import vis from './images/vis.gif'
+import vis2 from './images/vis2.gif'
 
 const Body_1 = () => {
    return (
     <span>
         <div className={'bg-white dark:bg-black-500 ' + styles.container}>
           <div className={styles.flexbox}>
-          <div className={'p-8 '  + styles.image}>
-            <Image src={vis} alt='pink blue and white 3d representaion of a online admin control dashboard.' className={' ' + styles.imageSrc}/>
+            <div className={'p-8 hidden dark:block '  + styles.image}>
+              <Image src={vis2} alt='A rotating representation of unlabled 3d data. The colors used for the data ranges from white at the bottom to red at the top.' className={' ' + styles.imageSrc}/>
+            </div>
+            <div className={'p-8 block dark:hidden '  + styles.image}>
+              <Image src={vis} alt='A rotating representation of unlabled 3d data. The colors used for the data ranges from white at the bottom to red at the top.' className={' ' + styles.imageSrc}/>
+            </div>
+              <div className={' ' + styles.textcontainer}>
+              <h1 className={'dark:text-white ' + styles.text}><span className={'whitespace-nowrap '}>Data Visualization</span> <br/> with <span className={' ' }>Javascript</span> and <span className={' '}>Python</span></h1>
+              <br/>
+              <h2 className={'dark:text-white ' }>Using platforms like Three.js and Matplotlip.</h2>
+            </div>
           </div>
-            <div className={' ' + styles.textcontainer}>
-            <h1 className={'dark:text-white ' + styles.text}> <span className={'whitespace-nowrap '}> Data Visualization</span> <br/> with <span className={' ' }>Javascript</span> and <span className={' '}>Python</span></h1>
-            <br/>
-            <h2 className={'dark:text-white ' }>Using platforms like Three.js and Matplotlip.</h2>
-          </div>
-      </div>
         </div>
     </span>
   )
