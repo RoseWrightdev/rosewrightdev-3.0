2bdd81296ececfb2e1e2b19cba0bfb988282d9f6
rosepetaldrift <rosesophiewright@gmail.com>
2022-12-18 15:01:19 -0600

    tabs

--- layout/Body_1/Body_1.jsx
+++ layout/Body_1/Body_1.jsx
@@ -1,5 +1,7 @@
-//next
+import React, { useState } from 'react';
+
 import Image from 'next/image'
+
 
 //styles
 import styles from './styles.module.css'
@@ -14,24 +16,70 @@ import vis from './images/vis.gif'
 import vis2 from './images/vis2.gif'
 
 const Body_1 = () => {
-   return (
-    <span>
-        <div className={'bg-white dark:bg-black-500 ' + styles.container}>
-          <div className={styles.flexbox}>
-            <div className={'p-8 hidden dark:block '  + styles.image}>
-              <Image src={vis2} alt='A rotating representation of unlabled 3d data. The colors used for the data ranges from white at the bottom to red at the top.' className={' ' + styles.imageSrc}/>
+
+    const PythonAlt = ''
+    const [currentTab, setCurrentTab] = useState('1');
+    const tabs = [
+        {
+            id: 1,
+            tabTitle: 'MatPlotLib',
+            title: 'Data Visualization',
+            title2: 'with Python',
+            content: '1',
+            srcLight: vis,
+            srcDark: vis2,
+            altText: 'A rotating representation of unlabled 3d data. The colors used for the data ranges from white at the bottom to red at the top.',
+        },
+        {
+            id: 2,
+            tabTitle: 'Three.js',
+            title: 'Data Visualization',
+            title2: '2',
+            content: '2'
+        },
+        {
+          id: 3,
+          tabTitle: 'Tab 3',
+          title: 'Data Visualization',
+          title2: '3',
+          content: '3'
+      }
+    ];
+
+    const handleTabClick = (e) => {
+        setCurrentTab(e.target.id);
+    }
+
+    return (
+        <span>
+            <div className={'flex justify-between	' + styles.tabs}>
+                {tabs.map((tab, i) =>
+                    <button className={'bg-white dark:bg-black-800 dark:hover:text-white dark:text-white text-black-900 hover:bg-gray-800 dark:hover:bg-black-600 disabled:hover:border-l-black-500 ' + styles.tabButton} key={i} id={tab.id} disabled={currentTab === `${tab.id}`} onClick={(handleTabClick)}>{tab.tabTitle}</button>
+                )}
             </div>
-            <div className={'p-8 block dark:hidden '  + styles.image}>
-              <Image src={vis} alt='A rotating representation of unlabled 3d data. The colors used for the data ranges from white at the bottom to red at the top.' className={' ' + styles.imageSrc}/>
+            <div>
+                {tabs.map((tab, i) =>
+                    <div key={i}>
+                        {currentTab === `${tab.id}` && <div><div className={'bg-white dark:bg-black-500 ' + styles.container}>
+                        <div className={styles.flexbox}>
+                          <div className={'p-8 hidden dark:block '  + styles.image}>
+                            <Image src={tab.srcDark} alt={tab.altText} className={' ' + styles.imageSrc}/>
+                          </div>
+                              <div className={'p-8 block dark:hidden '  + styles.image}>
+                                <Image src={tab.srcLight} alt={tab.altText} className={' ' + styles.imageSrc}/>
+                              </div>
+                                <div className={' ' + styles.textcontainer}>
+                                <h1 className={'dark:text-white ' + styles.text}><span className={'whitespace-nowrap '}>{tab.title}</span> <br/> {tab.title2}</h1>
+                                <br/>
+                                <h2 className={'dark:text-white ' }>{tab.content}</h2>
+                              </div>
+                            </div>
+                          </div></div>}
+                    </div>
+                )}
             </div>
-              <div className={' ' + styles.textcontainer}>
-              <h1 className={'dark:text-white ' + styles.text}><span className={'whitespace-nowrap '}>Data Visualization</span> <br/> with <span className={' ' }>Javascript</span> and <span className={' '}>Python</span></h1>
-              <br/>
-              <h2 className={'dark:text-white ' }>Using platforms like Three.js and Matplotlip.</h2>
-            </div>
-          </div>
-        </div>
-    </span>
-  )
+        </span>
+    );
 }
+
 export default Body_1;
