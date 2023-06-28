bf3499c5636d7ea61d0b92374fc91dd245cdb9de
Rose Wright <rosesophiewright@gmail.com>
2023-06-28 10:30:10 -0500

    Reset

--- pages/Welcome/Welcome.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "./Navbar/Navbar";
-import Conclusion from "./Footer/Footer"
-
-const Welcome = () => {
-    <span>
-    <p>test welcome</p>
-    </span>
-  }
-  export default Welcome;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import Head from 'next/head'
-import Welcome from './Welcome/Welcome.jsx'
+import Layout from './layout/layout'
+import Thesis from './layout/Thesis/Thesis'
+import Body_1 from './layout/Body_1/Body_1'
+import Body_2 from './layout/Body_2/Body_2'
+import Body_3 from './layout/Body_3/Body_3'
+import Conclusion from './layout/Conclusion/Conclusion'
+
 export default function Home() {
   return (
-    <div>
+    <div className=''>
       <Head>
         <title>RoseWright.dev</title>
         <meta name="description" content="RoseWright.dev" />
         <link rel="icon" href="/rose2.ico" />
       </Head>
-      <main>
-      <Welcome/>
-      </main>
+      <Layout>
+        <Thesis />
+        <main>
+        <Body_1 />
+        <Body_2 />
+        <Body_3 />
+        <Conclusion />
+        </main>
+      </Layout>
     </div>
   )
 }
