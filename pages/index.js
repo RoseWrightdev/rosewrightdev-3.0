a17f3117358c1d454e5f606d0f62132463c38f36
Rose Wright <rosesophiewright@gmail.com>
2023-06-28 03:06:00 -0500

    Move Layout and Hooks Folder into pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import Layout from '../layout/layout'
-import Thesis from '../layout/Thesis/Thesis'
-import Body_1 from '../layout/Body_1/Body_1'
-import Body_2 from '../layout/Body_2/Body_2'
-import Body_3 from '../layout/Body_3/Body_3'
-import Conclusion from '../layout/Conclusion/Conclusion'
+import Layout from './layout/layout'
+import Thesis from './layout/Thesis/Thesis'
+import Body_1 from './layout/Body_1/Body_1'
+import Body_2 from './layout/Body_2/Body_2'
+import Body_3 from './layout/Body_3/Body_3'
+import Conclusion from './layout/Conclusion/Conclusion'
 
 export default function Home() {
   return (
