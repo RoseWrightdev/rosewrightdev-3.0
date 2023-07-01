23e4df5ab16a49e0925409ecb1fc4b513710cd1b
Rose Wright <rosesophiewright@gmail.com>
2023-07-01 16:10:47 -0500

    Alt FIX

--- pages/layout/Conclusion/Conclusion.jsx
+++ pages/layout/Conclusion/Conclusion.jsx
@@ -18,9 +18,6 @@ const Conclusion = () => {
             title: '',
             title2: 'talk to me',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo nec ultrices dui sapien. Sit amet volutpat consequat mauris nunc. Id consectetur purus ut faucibus pulvinar elementum integer enim. Elit pellentesque habitant morbi tristique senectus et. Quam viverra orci sagittis eu volutpat odio facilisis mauris.',
-            srcLight: '',
-            srcDark: '',
-            altText: 'A rotating representation of unlabled 3d data. The colors used for the data ranges from white at the bottom to red at the top.',
         },
         {
             id: 2,
@@ -28,9 +25,6 @@ const Conclusion = () => {
             title: 'Data Visualization',
             title2: 'with Matplotlib.',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo nec ultrices dui sapien. Sit amet volutpat consequat mauris nunc. Id consectetur purus ut faucibus pulvinar elementum integer enim. Elit pellentesque habitant morbi tristique senectus et. Quam viverra orci sagittis eu volutpat odio facilisis mauris.',
-            srcLight: '',
-            srcDark: '',
-            altText: 'A rotating representation of unlabled 3d data. The colors used for the data ranges from white at the bottom to red at the top.',
         },
     ];
 
@@ -53,10 +47,8 @@ const Conclusion = () => {
                         {currentTab === `${tab.id}` && <div><div className={'bg-white dark:bg-black-800 ' + styles.container}>
                         <div className={styles.flexbox}>
                             <div className={'p-8 hidden dark:block '  + styles.image}>
-                            <Image src={tab.srcDark} alt={tab.altText} className={' ' + styles.imageSrc}/>
                             </div>
                                 <div className={'p-8 block dark:hidden '  + styles.image}>
-                                    <Image src={tab.srcLight} alt={tab.altText} className={' ' + styles.imageSrc}/>
                                 </div>
                                 <div className={' ' + styles.textcontainer}>
                                     <h1 className={'dark:text-white ' + styles.text}>{tab.title} {tab.title2}</h1>
